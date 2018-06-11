b4288c8ed53be0e5eca5c94f1529ad22e60f831b
Mathisfun123 <sairajareddykurelli@gmail.com>
2018-06-11 16:58:49 -0500

    Finished Coins? OutPut Explained - Greedy

--- src/CoinChangeAt1.java
+++ src/CoinChangeAt1.java
@@ -1,19 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoinChangeAt1 {
     private  static int least = Integer.MAX_VALUE;
+    private  static String leastCoins;
     public static void main(String[] args) {
         int coins1 []= {1,3,15,16};
         int appre [] = new int[coins1.length];
         solve(18,coins1,0, appre);
-        System.out.println(least);
+        System.out.println(least );
+        System.out.println(leastCoins);
+        /*
+        OutPut explained:
+        First Line : Least AMT coins to make target amt
+        Second Line: List of coins needed to make target amt
+         */
     }
-    public static void solve(int amt, int coins [], int index , int appreaSoFar[]) {
+    private static void solve(int amt, int coins [], int index , int appreaSoFar[]) {
         if (amt == 0) {
             int sum = 0;
             for (int i = 0; i < appreaSoFar.length; i++) {
@@ -21,7 +25,14 @@ public class CoinChangeAt1 {
                 //System.out.print("Values: "+  appreaSoFar[i] + " ");
             }
             //System.out.println();
-            least = Math.min(sum, least);
+            if(sum< least ) {
+                StringBuilder item = new StringBuilder();
+                for(int i: appreaSoFar){
+                    item.append(i);
+                }
+                leastCoins= item.toString();
+                least = sum;
+            }
         }
         if (index < coins.length) {
              int poss = amt / coins[index];
