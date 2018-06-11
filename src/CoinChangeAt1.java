e151da1110c46f05071ffe476b7b69d0e1824345
Mathisfun123 <sairajareddykurelli@gmail.com>
2018-06-11 16:21:13 -0500

    Finished Coins? - Greedy

--- src/CoinChangeAt1.java
+++ src/CoinChangeAt1.java
@@ -1,22 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class CoinChangeAt1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Integer> coinValues = new ArrayList<>();
+    private  static int least = Integer.MAX_VALUE;
+    public static void main(String[] args) throws FileNotFoundException {
+        int coins1 []= {1,5,10,20};
+        int appre [] = new int[coins1.length];
+        solve(25,coins1,0, appre);
+        System.out.println(least);
     }
-    public static void solve(int amt, ArrayList<Integer> Coinitms){
-        int[] upToamt = new int[amt+1];
-        int possEach[] = new int[amt+1];
-        upToamt[0]= 0;
-        for(int i = 1; i< upToamt.length; i++){
-            upToamt[i]= Integer.MAX_VALUE;
+    public static void solve(int amt, int coins [], int index , int appreaSoFar[]){
+        if(amt == 0 ){
+            int sum = 0;
+            for (int i = 0; i < appreaSoFar.length; i++) {
+                sum+= appreaSoFar[i];
+                //System.out.print("Values: "+  appreaSoFar[i] + " ");
+            }
+            //System.out.println();
+            least = Math.min(sum, least);
         }
-
-
+        int poss = amt/ coins[index];
+        for(int i = 0; i<= poss; i++){
+            appreaSoFar[index]= i;
+            solve(amt- (coins[index] * i), coins, index+1, appreaSoFar);
+        }
     }
 }
