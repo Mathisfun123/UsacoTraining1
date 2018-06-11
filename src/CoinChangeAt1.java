e43671d8f3a72640406ef82e1cf6896240d1de84
Mathisfun123 <sairajareddykurelli@gmail.com>
2018-06-11 16:36:52 -0500

    Finished Coins? - Greedy

--- src/CoinChangeAt1.java
+++ src/CoinChangeAt1.java
@@ -7,26 +7,28 @@ import java.util.TreeSet;
 
 public class CoinChangeAt1 {
     private  static int least = Integer.MAX_VALUE;
-    public static void main(String[] args) throws FileNotFoundException {
-        int coins1 []= {1,5,10,20};
+    public static void main(String[] args) {
+        int coins1 []= {1,3,15,16};
         int appre [] = new int[coins1.length];
-        solve(25,coins1,0, appre);
+        solve(18,coins1,0, appre);
         System.out.println(least);
     }
-    public static void solve(int amt, int coins [], int index , int appreaSoFar[]){
-        if(amt == 0 ){
+    public static void solve(int amt, int coins [], int index , int appreaSoFar[]) {
+        if (amt == 0) {
             int sum = 0;
             for (int i = 0; i < appreaSoFar.length; i++) {
-                sum+= appreaSoFar[i];
+                sum += appreaSoFar[i];
                 //System.out.print("Values: "+  appreaSoFar[i] + " ");
             }
             //System.out.println();
             least = Math.min(sum, least);
         }
-        int poss = amt/ coins[index];
-        for(int i = 0; i<= poss; i++){
-            appreaSoFar[index]= i;
-            solve(amt- (coins[index] * i), coins, index+1, appreaSoFar);
-        }
+        if (index < coins.length) {
+             int poss = amt / coins[index];
+             for (int i = 0; i <= poss; i++) {
+                    appreaSoFar[index] = i;
+                    solve(amt - (coins[index] * i), coins, index + 1, appreaSoFar);
+             }
+         }
     }
 }
